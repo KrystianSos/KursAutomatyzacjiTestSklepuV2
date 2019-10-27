429b2dd99021da284f4a0b0471eb99f0a8b9b7f0
ala makota <kw.dziubinska@gmail.com>
2019-10-27 14:01:00 +0100

    1st test

--- src/main/java/models/Product.java
+++ src/main/java/models/Product.java
@@ -12,7 +12,6 @@ public class Product {
 
     private int quantity;
 
-
     public BigDecimal getTotalPrice() {
         return totalPrice;
     }
--- src/test/java/products/ProductTests.java
+++ src/test/java/products/ProductTests.java
@@ -2,10 +2,23 @@ package products;
 
 import base.BaseTest;
 import org.testng.annotations.Test;
+import pages.commons.MainPage;
+import pages.products.ProductMiniaturePage;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 public class ProductTests extends BaseTest {
 
     @Test
     public void displayProductsWithPricesOver20Usd() {
+        MainPage mainPage = new MainPage(getDriver());
+        List<ProductMiniaturePage> allProducts = mainPage.getProductContainer().getProducts();
+
+        for (ProductMiniaturePage product : allProducts) {
+            if(product.getPrice().compareTo(new BigDecimal(20)) > 0){
+                System.out.println(product.getName());
+            }
+        }
     }
 }
